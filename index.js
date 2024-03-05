8ca79b3057bd4811cc23b8e4a7fb1861a8264899
Nada-123 <nada.alshakiry@gmail.com>
2024-03-05 18:56:02 +0300

    Mission Accomplished

---
+++ index.js
@@ -0,0 +1,15 @@
+const divide = () => {
+    return (2000 / 100);
+}
+
+const square = (par) => {
+    return par * par;
+};
+
+const add = (par1,par2) => {
+	return par1+ par2;
+};
+
+divide();
+square(2);
+add(2, 1);
